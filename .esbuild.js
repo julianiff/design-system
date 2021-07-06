d1c4774693ad6b67afa9c303a4b76810c81288a3
julianiff <julian.iff@helsana.ch>
2021-07-06 11:02:47 +0200

    update esbuild script to create inependent bundles

--- .esbuild.js
+++ .esbuild.js
@@ -1,9 +1,29 @@
 import {build} from 'esbuild';
 import {litCssPlugin} from 'esbuild-plugin-lit-css';
+import fs from 'fs';
+import rimraf from 'rimraf';
 
+const OUTPUT_DIR = './lib';
+
+// Read index.ts and build bundles
+const buildAsIndependentBundles = (indexDir = './src/index.ts') => {
+  const data = fs.readFileSync(indexDir, 'UTF-8');
+  const lines = data.split(/\r?\n/);
+  const entries = lines
+    .map((line) => line.split("'"))
+    .filter((item) => item != '')
+    .map((item) => `./src${item[1].replace('.', '')}`);
+  console.log('Building with following Bundles', entries);
+  return [indexDir, ...entries];
+};
+
+// Cleanup Build Dir
+rimraf.sync(OUTPUT_DIR);
+
+//Build Independent Bundles
 build({
-  entryPoints: ['./src/index.ts'],
-  outdir: './lib',
+  entryPoints: buildAsIndependentBundles(),
+  outdir: OUTPUT_DIR,
   bundle: true,
   minify: true,
   sourcemap: true,
@@ -12,10 +32,11 @@ build({
   plugins: [litCssPlugin({uglify: true})],
 });
 
+// Build Styling
 build({
   entryPoints: ['./src/styling/design.css'],
-  outfile: './lib/index.css',
-  bundle: true,
+  outfile: `${OUTPUT_DIR}/index.css`,
+  bundle: false,
   minify: true,
   color: true,
 });
